78e59b59e14eedf6abba855057d0d437a39b794b
juanse217 <juansebas08004@gmail.com>
2025-09-27 17:07:35 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+##
+
+## ONLINE GAME PLAYERS SYSTEM.
+In this project we create the different parts of a game. We focus on the DS for different situations in the game. 
+
+We'll have a "Player" class where all the data of our players will be saved. 
---
+++ src/main/java/com/sebastian/model/Player.java
@@ -0,0 +1,54 @@
+package com.sebastian.model;
+
+public class Player {
+    private String nickname; 
+    private int level; 
+    private int points;
+    public String getNickname() {
+        return nickname;
+    }
+    
+    public int getLevel() {
+        return level;
+    }
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    public int getPoints() {
+        return points;
+    }
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nickname == null) ? 0 : nickname.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Player other = (Player) obj;
+        if (nickname == null) {
+            if (other.nickname != null)
+                return false;
+        } else if (!nickname.equals(other.nickname))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Player [nickname=" + nickname + ", level=" + level + ", points=" + points + "]";
+    } 
+
+}
